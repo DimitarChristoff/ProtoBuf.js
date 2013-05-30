d686381f212b21935acb4e661c17aaf66ff2f322
Dimitar Christoff <dchristoff@rulefinancial.com>
2013-05-30 17:29:46 +0100

    faker.js for random data

--- cjs-dto-test.js
+++ cjs-dto-test.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 var express = require('express'),
-	app = express(),
-	http = require('http'),
-	server = http.createServer(app),
-	ws = new (require('ws').Server)({server: server}),
-	fs = require('fs'),
-	ByteBuffer = require('bytebuffer');
-
+    app = express(),
+    http = require('http'),
+    server = http.createServer(app),
+    ws = new (require('ws').Server)({server: server}),
+    fs = require('fs'),
+    ByteBuffer = require('bytebuffer'),
+    Faker = require('Faker');
 
 
 var DTOs = require('./ProtoBuf').newBuilder().import({
@@ -32,40 +32,35 @@ var DTOs = require('./ProtoBuf').newBuilder().import({
     "imports": [],
     "options": {}
 }).import({
-    "package": null,
-    "messages": [],
-    "enums": [],
-    "imports": [],
-    "options": {},
-    "extend": [
-        {
-            "name": "Foo",
-            "fields": [
-                {
-                    "rule": "optional",
-                    "type": "string",
-                    "name": "buzz",
-                    "id": 3,
-                    "options": {}
-                }
-            ],
-            "enums": [],
-            "messages": [],
-            "options": {}
-        }
-    ]
-}).build();
-
-
-
+        "package": null,
+        "messages": [],
+        "enums": [],
+        "imports": [],
+        "options": {},
+        "extend": [
+            {
+                "name": "Foo",
+                "fields": [
+                    {
+                        "rule": "optional",
+                        "type": "string",
+                        "name": "buzz",
+                        "id": 3,
+                        "options": {}
+                    }
+                ],
+                "enums": [],
+                "messages": [],
+                "options": {}
+            }
+        ]
+    }).build();
 
 
 var foo = new DTOs.Foo({
-	fizz: 'fizz',
+    fizz: 'fizz',
     buzz: 'buzz'
 });
-
-
 
 
 app.use(express.static(__dirname + '/'));
@@ -83,23 +78,28 @@ server.listen(3333);
 
 console.log(foo);
 
-var ab = foo.toArrayBuffer();
+
+// var ab = foo.toArrayBuffer();
 // var bb = new ByteBuffer();
 // foo.encode(bb);
 // console.log(ab);
 // console.log(Buffer.isBuffer(ab));
 
 ws.on('connection', function(ws){
-	// console.log(foo.toArrayBuffer() instanceof ArrayBuffer);
-	// var b = new Buffer(foo, "utf8");
-	// var bb = new ByteBuffer().wrap(b);
-	// foo.encode(bb);
+    console.log(foo.toArrayBuffer() instanceof ArrayBuffer);
+    // var b = new Buffer(foo, "utf8");
+    // var bb = new ByteBuffer().wrap(b);
+    // foo.encode(bb);
     setInterval(function(){
-        foo.setFizz(new Date().getTime() + Math.random() * 10000);
-        console.log('sending ',  foo);
-        ws.send(foo.encode(), {binary: true, mask: false});
+        var ab;
+        foo.setFizz(Faker.Name.findName());
+        foo.setBuzz(new Date().getTime());
+        console.log('sending ', foo);
+        ab = foo.toArrayBuffer();
+        ws.send(ab, {binary: true, mask: true});
+        console.log(ab);
 
     }, 1000);
-	// console.log('connected');
+    // console.log('connected');
 });
 // console.log(bb.toArrayBuffer());
